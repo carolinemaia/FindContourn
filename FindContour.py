f5b7810b2a51ec4c3aaddf0f936bbc25dd5d6072
carolinemaia <37007813+carolinemaia@users.noreply.github.com>
2018-04-26 13:05:24 -0300

    Update FindContour.py
    Teste com video

--- FindContour.py
+++ FindContour.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import cv2
+import numpy as np
 
 biggest_contour = None
 biggest_area = 1
-imageFile='fachada.jpg'
-image = cv2.imread(imageFile)
-height, width, channels = image.shape
-imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  
-imgray = cv2.GaussianBlur(imgray, (5, 5), 0)
+#video
+cap = cv2.VideoCapture('videoo.mp4')
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+##imageFile='teste5.jpg'
+##image = cv2.imread(imageFile)
+##height, width, channels = image.shape
+##imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  
+##imgray = cv2.GaussianBlur(imgray, (5, 5), 0)
 
 # O código abaixo define um retângulo na região central com 20% do comprimento e 20% de altura
 MIN_THRESH = 0
@@ -29,8 +35,13 @@ def isExtCentral(extL,extR,extT,extB):
 while True:#Loop que processa a imagem aumentando o MIN_THRESH ou diminuindo CENTRAL_AREA 
 	if(CENTRAL_AREA<0.01):
 		break #interrompe a busca se a área central estiver tomando a imagem toda
+       #capture frame-frame
+	_, frame = cap.read()
+
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	blur = cv2.GaussianBlur(gray,(5,5),0)
+	_, thresh = cv2.threshold(blur, MIN_THRESH, 255, cv2.THRESH_BINARY)
 	
-	_, thresh = cv2.threshold(imgray, MIN_THRESH, 255, cv2.THRESH_BINARY)
 	_, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 	MIN_THRESH += 10 #aumenta o threshold mínimo para incluir mais áreas
 
@@ -62,15 +73,15 @@ while True:#Loop que processa a imagem aumentando o MIN_THRESH ou diminuindo CEN
 
 
 #desenha quadrado que corresponde a região central
-cv2.rectangle(image, (MIN_X, MIN_Y), (MAX_X, MAX_Y), (0, 255, 0), 3)
+cv2.rectangle(frame, (MIN_X, MIN_Y), (MAX_X, MAX_Y), (0, 255, 0), 3)
 
 moments = cv2.moments(biggest_contour)
 cx = int(moments['m10'] / biggest_area)
 cy = int(moments['m01'] / biggest_area)
-cv2.circle(image, (cx, cy), 5, (255, 255, 0), -1)  # desenha um ponto azul no centro das figuras selecionadas
+cv2.circle(frame, (cx, cy), 5, (255, 255, 0), -1)  # desenha um ponto azul no centro das figuras selecionadas
 
-cv2.drawContours(image, [biggest_contour], -1, (0, 255, 255), 2)  # desenha borda amarela na figura de maior área
+cv2.drawContours(frame, [biggest_contour], -1, (0, 255, 255), 2)  # desenha borda amarela na figura de maior área
 cv2.imshow('Threshold', thresh)
-cv2.imshow('Maior contorno', image)
+cv2.imshow('Frame',frame)
 cv2.waitKey()
 cv2.destroyAllWindows()
